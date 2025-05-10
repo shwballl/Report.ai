4956881b42060080b9ce4b9c01c2015e8da98ac5
mac <mac@macs-MacBook-Pro.local>
2025-05-10 14:55:40 +0300

    Release 0.0.1. Fix Docker issues. Fix frontend.

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # change to in the future ["http://localhost:3000"]
+    allow_origins=["http://localhost:80"],  # change to in the future ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
